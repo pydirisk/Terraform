bfb72b088f57189ddc6c19b6e674461684b5bd63
pydirisk <prasanthkumar.pydi@gmail.com>
2023-06-20 10:25:03 +0530

    data resource

---
+++ 01 -Sample/03-data/data.tf
@@ -0,0 +1,9 @@
+data "aws_ami" "pydi" {
+    most_recent = true
+    name_regex  = "Centos-8-DevOps-Practice"
+    owners      = ["256111340838"]
+}
+
+output "aws_id" {
+    value = data.aws_ami.ami.image_id
+}
