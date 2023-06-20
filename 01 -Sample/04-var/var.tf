625c96aac99428e74f812e581d4372426e21d593
pydirisk <prasanthkumar.pydi@gmail.com>
2023-06-20 11:45:44 +0530

    latest update

--- 01 -Sample/04-var/var.tf
+++ 01 -Sample/04-var/var.tf
@@ -13,3 +13,23 @@ variable "sample1" {
 output "sample1" {
     value = var.sample1
 }
+
+variable "sample2" {
+    default = ["hello",1000,true,"world"]
+}
+
+variable "sample3" {
+    default = {
+        string = "hello"
+        number = 100
+        boolean = true
+    }
+}
+
+variable "sample4" {
+    default = "hello"
+}
+
+output "types"{
+    value = "variable sample4 - ${var.sample4}, First value in list - ${var.sample2[0]}, Boolean value of map = ${var.sample3["boolean"]}" 
+}
